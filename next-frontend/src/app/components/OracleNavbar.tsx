d591b78fc98f4fb477287d801952914e51c6e8f0
Ryan Willson <willsonr8@gmail.com>
2025-03-12 15:06:00 -0400

    Adds basic navbar

---
+++ next-frontend/src/app/components/OracleNavbar.tsx
@@ -0,0 +1,26 @@
+import React from 'react';
+import { Navbar, NavbarBrand, NavbarContent, NavbarItem, Link, Button } from '@heroui/react';
+
+export default function OracleNavbar() {
+
+  return (
+    <div className="w-screen h-screen p-8 flex items-start justify-center">
+    <Navbar isBordered>
+        <NavbarBrand>
+          <p className='font-bold text-inherit'>THE ORACLES</p>
+        </NavbarBrand>
+        <NavbarContent className='hidden sm:flex gap-4' justify='center'>
+          <NavbarItem>
+            <Link href='/'>Home</Link>
+          </NavbarItem>
+          <NavbarItem>
+            <Link href='/about'>About</Link>
+          </NavbarItem>
+          <NavbarItem>
+            <Link href='/contact'>Contact</Link>
+          </NavbarItem>
+        </NavbarContent>
+      </Navbar>
+      </div>
+  );
+}
---
+++ next-frontend/src/app/page.jsx
@@ -0,0 +1,10 @@
+'use client'
+
+import React from 'react';
+import OracleNavbar from './components/OracleNavbar.tsx';
+export default function Page() {
+  
+  return (
+    <OracleNavbar />
+  );
+}
